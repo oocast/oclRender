231d1e6e739cfa5e2ff8eb8d0e93aa72a28e5259
crmxm <crmcrm1992@gmail.com>
2015-06-10 04:15:16 +0800

    scene.cpp & test_render.cpp modified, ver 1.93

--- src/rasterizerC/test_render.cpp
+++ src/rasterizerC/test_render.cpp
@@ -1,5 +1,7 @@
 #include "oclrender.h"
 
+const float PI=3.14159f;
+
 using namespace std;
 void TestPicture()
 {
@@ -21,7 +23,7 @@ void TestPicture()
     scene.Add(new ConvexPoly(LineSegment(Vector(0.1F, .0F), Vector(0.1F, 1.0F), 0.01F, &lsColor)));
     scene.Add(new ConvexPoly(LineSegment(Vector(0.2F, .0F), Vector(0.2F, 1.0F), 0.01F, &lsColor)));
     scene.Add(new ConvexPoly(LineSegment(Vector(0.3F, .0F), Vector(0.3F, 1.0F), 0.01F, &lsColor)));
-    scene.Add(new ConvexPoly(LineSegment(Vector(0.4F, .0F), Vector(0.4F, 1.0F), 0.01F, &lsColor).Transformation(Around(Vector(0.5F,0.5F),Rotate(1.57F)))));
+    scene.Add(new ConvexPoly(LineSegment(Vector(0.4F, .0F), Vector(0.4F, 1.0F), 0.01F, &lsColor).Transformation(Around(Vector(0.5F,0.5F),Rotate(PI /2)))));
 
     //ConvexPoly some(LineSegment(Vector(0.4, 0), Vector(0.4, 1), 0.01, &lscolor));
     Color blue = InitColor(.0, .0, 1.0, 0.5);
@@ -33,11 +35,11 @@ void TestPicture()
     Vector CC(294.0F / 512.0F, 1.0F - 183.0F / 512.0F);
     Vector HH(403.0F / 512.0F, 1.0F - 163.0F / 512.0F);
     ConvexPoly blackBar(Rectangle(LL, HH, &black));
-    ConvexPoly leanBar(blackBar.Transformation(Around(CC, Rotate(3.14159F / 13.0F))));
-    scene.Add(new ConvexPoly(blackBar.Transformation(Around(CC, Rotate(3.14159F / 13.0F)))));
+    ConvexPoly leanBar(blackBar.Transformation(Around(CC, Rotate(PI / 13.0F))));
+    scene.Add(new ConvexPoly(blackBar.Transformation(Around(CC, Rotate(PI / 13.0F)))));
     Ellipse circle(Circle(Vector(0.75F, 0.1F), 0.5F, &green));
 
-    Transform rot(Rotate(3.14159F * (1.0F / 2.0F + 1.0F / 8.0F)));
+    Transform rot(Rotate(PI * (1.0F / 2.0F + 1.0F / 8.0F)));
     Transform sca(Scale(1.0F, 0.2F));
     //Ellipse ellip(circle.Transformation(Around(Vector(0.75F, 0.1F), rot * sca)));
     scene.Add(new Ellipse(circle.Transformation(Around(Vector(0.75F, 0.1F), rot * sca))));
@@ -52,18 +54,7 @@ void TestPicture()
     
     CLInit(fileName, kernelName);
 
-    CLReadImageBuff(memObj[0], bg);
-    //image_modifyio(scene, bg);
     scene.Draw(bg);
-    CLWriteImageBuff(memObj[0], bg);
-    /*
-    int size=bg.width*bg.height*sizeof(Color);
-    memObj[0]=clCreateBuffer(context, CL_MEM_READ_WRITE|CL_MEM_COPY_HOST_PTR, size, &bg.pixels[0],NULL);
-
-    scene.draw(bg);
-
-    err=clEnqueueReadBuffer(cmd_queue, memObj[0], CL_TRUE, 0, size, &bg.pixels[0], 0, NULL, NULL);
-    */
 
     CLRelease();
     //image.write_ppm(f, &bg);
