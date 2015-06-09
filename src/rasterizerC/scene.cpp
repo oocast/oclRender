8f05b3c68008540cca3fd9fd688cb6d62b3c0ca5
crmxm <crmcrm1992@gmail.com>
2015-06-10 04:10:00 +0800

    scene.cpp & test_render.cpp modified, ver 1.93

--- src/rasterizerC/scene.cpp
+++ src/rasterizerC/scene.cpp
@@ -1,4 +1,5 @@
 #include "scene.h"
+#include "cl_helper.h"
 
 void Scene::
 Add(Shape * node)
@@ -9,7 +10,9 @@ Add(Shape * node)
 void Scene::
 Draw(PPMImage & image, int superSampling)
 {
+    CLReadImageBuff(memObj[0], image);
     for (size_t i = 0; i < nodes.size(); i++) {
         nodes[i]->Draw(image, superSampling);
     }
+    CLWriteImageBuff(memObj[0], image);
 }
